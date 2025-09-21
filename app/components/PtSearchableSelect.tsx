a3ca961ef73930cd89a6e459252efb627e209044
azharanggakusuma <azharanggakusuma01@gmail.com>
2025-09-21 18:44:53 +0700

    update PtSearchableSelect.tsx

--- app/components/PtSearchableSelect.tsx
+++ app/components/PtSearchableSelect.tsx
@@ -173,7 +173,7 @@ export const PtSearchableSelect = ({ value, onChange, placeholder = "Ketik untuk
                 </div>
             )}
 
-            <AnatePresence>
+            <AnimatePresence>
                 {isOpen && (
                      <Portal>
                         <motion.div
@@ -206,7 +206,7 @@ export const PtSearchableSelect = ({ value, onChange, placeholder = "Ketik untuk
                         </motion.div>
                     </Portal>
                 )}
-            </AnatePresence>
+            </AnimatePresence>
         </div>
     );
 };
